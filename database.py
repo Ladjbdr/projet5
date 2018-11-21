2c3f423ee1c5b7acd1d438103c6e66319f6fd5b2
Kitch <ladj.dr@gmail.com>
2018-11-21 19:34:09 +0100

    minor change in database.py

--- database.py
+++ database.py
@@ -27,11 +27,27 @@ def create_row(conn, sql_statement, entries):
 
 def main():
     conn = create_connection()
-    categories = get_categories()
-    category = [(id, category['name']) for id, category in enumerate(categories, 1)]
+    categories_ = get_categories()
+    category = [(id, category['name']) for id, category in enumerate(categories_, 1)]
     grades = ('a', 'b', 'c', 'd', 'e')
     nutrition_grade = [(id, grade) for id, grade in enumerate(grades, 1)]
-    
+    product = []
+    for idx in range(len(categories_)):
+        products = get_category_products(categories_, idx + 1)
+        element = [(product['code'],
+				  product['product_name_fr'],
+				  product['ingredients_text_fr'],
+				  product['stores'],
+                  product['nutrition_grade_fr']) 
+				  for product in products 
+                  if 'nutrition_grade_fr' in product.keys() 
+                  and product['code'] 
+                  and product['product_name_fr'] 
+                  and product['ingredients_text_fr'] 
+                  and product['stores'] 
+                  and product['nutrition_grade_fr'] != '']
+        product += element
+
     with conn:
         create_table(conn, CREATE_CATEGORIES)
         create_table(conn, CREATE_PRODUCTS)
@@ -39,25 +55,7 @@ def main():
         create_table(conn, CREATE_PRODUCT_CATEGORY)
         create_row(conn, INSERT_INTO_CATEGORIES, category)
         create_row(conn, INSERT_INTO_NUTRITION_GRADE, nutrition_grade)
-
-    for idx in range(len(categories)):
-        products = get_category_products(categories, idx + 1)
-        #category = [(id, category['name']) for id, category in enumerate(categories, 1)]
-        product = [(product['code'],
-				  product['product_name_fr'],
-				  product['ingredients_text_fr'],
-				  product['stores'],
-                  product['nutrition_grade_fr']
-                  ) 
-				  for product in products if 'nutrition_grade_fr' in product.keys() and 
-				  								  product['code'] and 
-				  								  product['product_name_fr'] and 
-				  								  product['ingredients_text_fr'] and 
-												  product['stores'] and
-                                                  product['nutrition_grade_fr'] != '']
-        with conn:
-            create_row(conn, INSERT_INTO_PRODUCTS, product)
-
+        create_row(conn, INSERT_INTO_PRODUCTS, product)
 
 
 if __name__=='__main__':
